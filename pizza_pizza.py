1396538d55a108605eee41184a71be85b2ce6e5f
Freshnick71 <48777156+Freshnick71@users.noreply.github.com>
2021-11-06 22:23:06 -0400

    new commi

--- pizza_pizza.py
+++ pizza_pizza.py
@@ -5,6 +5,7 @@ class Pizza:
         self.cheese = 's'
         self.meats = set()
         self.veggies = set()
+pizza = Pizza()
 
 class Topping:
     __slots__ = ['code', 'name', 'price']
@@ -14,7 +15,23 @@ class Topping:
         self.name = name
         self.price = price
 
+_CHEESES = {'f': Topping('f', 'Fresh Mozzeralla', 1.5), 's': Topping('s', 'Shredded Cheese', 1.0), 'c': Topping('c', 'Cheddar Cheese', 0.5)}
+_MEATS = {}
+_VEGGIES = {}
+def print_pizza(pizza):
+    print("One pizza with", _CHEESES[pizza.cheese].name, end = ',')
+    for meat in pizza.meats:
+        name = _MEATS[meat].name
+        if name != 'None':
+            print (name, end= ',')
+    for veggie in pizza.veggies:
+        name = _VEGGIES[veggie].name
+        if name != 'None':
+            print(name, end= ",")
+    print('\b\b : $', round(pizza.price, 2), sep = '')
 def main():
-    delicacy = Pizza()
-    print(delicacy)
+    pizza = Pizza()
+    print_pizza(pizza)
+    #menu = Topping()
+    #print(pizza)
 main()
